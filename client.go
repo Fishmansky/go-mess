1c2cda54dae7e77a9041a91828e45160c765a386
Fishmansky <paul96guitar@gmail.com>
2024-03-10 21:33:18 +0100

    changes in connection model - client keeps connection open

--- client.go
+++ client.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"net"
+	"time"
 )
 
 const (
 	RequestConnect = iota
+	RequestDisconnect
 	RequestPublish
 	RequestSubscribe
 )
@@ -20,7 +22,9 @@ type Request struct {
 
 type Client struct {
 	name       string
+	authKey    string
 	serverAddr string
+	session    net.Conn
 }
 
 func NewClient(name string, serverAddr string) *Client {
@@ -31,7 +35,7 @@ func NewClient(name string, serverAddr string) *Client {
 }
 
 func (c *Client) Send(r *Request) {
-	data, err := json.Marshal(r)
+	data, err := json.Marshal(&r)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -52,33 +56,46 @@ func (c *Client) Send(r *Request) {
 }
 
 func (c *Client) Connect() {
+	conn, err := net.Dial("tcp", c.serverAddr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	c.session = conn
 	r := Request{
 		Type:    RequestConnect,
 		Sender:  c.name,
-		Payload: "password:123123;role=publisher",
+		Payload: c.authKey,
 	}
 	c.Send(&r)
 }
 
-func (c *Client) Publish() {
+func (c *Client) Close() {
 	r := Request{
-		Type:    RequestPublish,
+		Type:    RequestDisconnect,
 		Sender:  c.name,
-		Payload: "Stormy weather is to be expected",
+		Payload: c.authKey,
 	}
 	c.Send(&r)
+	c.session.Close()
 }
 
-func (c *Client) Subsribe() {
+func (c *Client) Subscribe(q string) {
 	r := Request{
 		Type:    RequestSubscribe,
 		Sender:  c.name,
-		Payload: "weather-report",
+		Payload: q,
 	}
 	c.Send(&r)
+}
+
+func (c *Client) Receive(r *Request) {
+
 }
 
 func main() {
 	c := NewClient("Con", "localhost:8080")
 	c.Connect()
+	time.Sleep(time.Millisecond * 2000)
+	c.Subscribe("weather-report")
 }
--- go-mess.go
+++ go-mess.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	RequestConnect = iota
+	RequestDisconnect
 	RequestPublish
 	RequestSubscribe
 )
@@ -21,16 +22,23 @@ type Request struct {
 }
 
 type Publisher interface {
-	Publish(b []byte)
+	Publish(r *Request)
 }
 type Subscriber interface {
-	Receive(b []byte)
+	Subscribe(r *Request)
+	Receive(r *Request)
+}
+
+type Queue struct {
+	name        string
+	subscribers []string
 }
 
 type Server struct {
 	listenAddr  string
 	publishers  []Publisher
 	subscribers []Subscriber
+	queues      []Queue
 }
 
 func NewServer(listenAddr string) *Server {
@@ -38,7 +46,19 @@ func NewServer(listenAddr string) *Server {
 		listenAddr:  listenAddr,
 		publishers:  []Publisher{},
 		subscribers: []Subscriber{},
+		queues:      []Queue{},
 	}
+}
+
+func (s *Server) AddSub(r *Request) error {
+	for _, q := range s.queues {
+		if q.name == r.Payload {
+			q.subscribers = append(q.subscribers, r.Sender)
+			log.Printf("%s subscribed to %s\n", r.Sender, r.Payload)
+			return nil
+		}
+	}
+	return fmt.Errorf("Queue %s not found", r.Payload)
 }
 
 func (s *Server) Run() {
@@ -55,11 +75,14 @@ func (s *Server) Run() {
 			log.Fatal(err)
 			return
 		}
-		go handleConnection(conn)
+		go s.handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func (s *Server) Publish(r *Request) {
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
 	for {
@@ -72,20 +95,26 @@ func handleConnection(conn net.Conn) {
 		}
 		var r Request
 		json.Unmarshal(buffer[:n], &r)
-		handleRequest(&r)
+		s.handleRequest(&r)
 	}
 }
 
-func handleRequest(r *Request) {
+func (s *Server) handleRequest(r *Request) {
 	switch {
 	case r.Type == RequestConnect:
 		fmt.Printf("USER %s CONNECTED TO SERVER\n", r.Sender)
 		return
+	case r.Type == RequestDisconnect:
+		fmt.Printf("USER %s DISCONNECTED\n", r.Sender)
+		return
 	case r.Type == RequestPublish:
-		fmt.Printf("%s: %s\n", r.Sender, r.Payload)
+		fmt.Printf("%s published: %s\n", r.Sender, r.Payload)
 		return
 	case r.Type == RequestSubscribe:
-		fmt.Printf("%s subscribed to %s\n", r.Sender, r.Payload)
+		err := s.AddSub(r)
+		if err != nil {
+			log.Println(err)
+		}
 		return
 	}
 }
